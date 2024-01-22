79e995296d4dac3d692c3ff067d77170abb4ab90
Valentin Niess <niess@in2p3.fr>
2024-01-22 11:58:10 +0100

    Energy spectrum

--- src/include/PrimaryGenerator.hh
+++ src/include/PrimaryGenerator.hh
@@ -4,6 +4,8 @@
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "G4SystemOfUnits.hh"
 
+#include <array>
+
 class G4ParticleGun;
 class G4Event;
 
@@ -33,6 +35,22 @@ struct PrimaryGenerator : public G4VUserPrimaryGeneratorAction {
         PrimaryGenerator();
         ~PrimaryGenerator() override;
         G4ParticleGun * particleGun = nullptr;
+
+        std::array<std::pair<double, double>, 11> spectrum = {
+            // Po^218 -> Pb^214.
+            std::make_pair(0.242,  7.3),
+            std::make_pair(0.295, 18.4),
+            std::make_pair(0.352, 35.6),
+            // Pb^214 -> Bi^214.
+            std::make_pair(0.609, 45.5),
+            std::make_pair(0.768,  4.9),
+            std::make_pair(0.934,  3.1),
+            std::make_pair(1.120, 14.9),
+            std::make_pair(1.238,  5.8),
+            std::make_pair(1.378,  4.0),
+            std::make_pair(1.764, 15.3),
+            std::make_pair(2.204,  4.9),
+        };
 };
 
 #endif
